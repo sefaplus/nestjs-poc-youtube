4f58a4521eb1cddb46236cd64bf54429d4e5c8f5
GeorgiyI <izmailov.georgiy.fortech@gmail.com>
2022-09-27 14:51:03 +0300

    error handling on frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,33 @@
-import { Box, Button, Input, LinearProgress, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Input,
+  LinearProgress,
+  Modal,
+  Typography,
+} from "@mui/material";
 import { SxProps } from "@mui/system";
 import { ChangeEvent, useState } from "react";
 import { ChunkUpload } from "./libs/chunkUpload/chunkUpload.lib";
 const withMargin: SxProps = { margin: "1em" };
 function App() {
   const [file, setFile]: [File | undefined, Function] = useState(undefined);
-  const [isUploading, setIsUploading] = useState(false);
+  const [isUploading, setUploading] = useState(false);
   const [progress, setProgress] = useState(0);
+  const [isOpen, setOpen] = useState(false);
+  const [errText, setErrText] = useState("");
   async function handleUpload() {
     if (!file) return;
-    if (await ChunkUpload.sendChunks(file, 8192, setIsUploading, setProgress))
-      setIsUploading(false);
+    await ChunkUpload.sendChunks(file, 8192, setUploading, setProgress)
+      .then(() => {
+        setUploading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setUploading(false);
+        setErrText(err.message);
+        setOpen(true);
+      });
   }
 
   function handleInputChange(
@@ -28,7 +45,28 @@ function App() {
         rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
       />
-
+      <Modal
+        open={isOpen}
+        onClose={() => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        <Box
+          sx={{
+            width: "350px",
+            height: "200px",
+            backgroundColor: "white",
+            padding: "35px",
+            borderRadius: "35px",
+          }}
+        >
+          <Typography id="modal-modal-title">Error occured:</Typography>
+          <Typography id="modal-modal-title" sx={{ color: "red" }}>
+            {errText}
+          </Typography>
+        </Box>
+      </Modal>
       <Box
         sx={{
           width: "100%",
